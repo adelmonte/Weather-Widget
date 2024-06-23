478cd074eeac2eb83b1c63b1fb50b7996b8ab910
adelmonte <138840776+adelmonte@users.noreply.github.com>
2024-06-23 16:40:14 -0700

    Create forecast.sh

---
+++ forecast.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+
+CITY="Spokane"
+API_KEY="************************************"
+API_URL="http://api.openweathermap.org/data/2.5/forecast?q=${CITY}&cnt=24&appid=${API_KEY}&units=imperial"
+sleep 2
+
+get_weather_icon() {
+    case $1 in
+        Clear) echo "☀️";;
+        Clouds) echo "☁️";;
+        Rain) echo "🌧️";;
+        Snow) echo "❄️";;
+        Drizzle) echo "🌦️";;
+        Thunderstorm) echo "⛈️";;
+        Mist|Fog|Haze) echo "🌫️";;
+        *) echo "❓";;
+    esac
+}
+
+center_text() {
+    local text="$1"
+    local width="$2"
+    local textwidth=${#text}
+    local padding=$(( (width - textwidth) / 2 ))
+    local extra=$(( (width - textwidth) % 2 ))
+    printf "%*s%s%*s" $padding "" "$text" $(( padding + extra )) ""
+}
+
+print_centered_row() {
+    local col1="$1"
+    local col2="$2"
+    local col3="$3"
+    local width=15
+    printf "%s%s%s\n" "$(center_text "$col1" $width)" "$(center_text "$col2" $width)" "$(center_text "$col3" $width)"
+}
+
+get_weather() {
+    local response=$(curl -s $API_URL)
+    local day1_temp=$(echo $response | jq -r '.list[0].main.temp_max' | awk '{print int($1+0.5)}')
+    local day1_cond=$(echo $response | jq -r '.list[0].weather[0].main')
+    local day2_temp=$(echo $response | jq -r '.list[8].main.temp_max' | awk '{print int($1+0.5)}')
+    local day2_cond=$(echo $response | jq -r '.list[8].weather[0].main')
+    local day3_temp=$(echo $response | jq -r '.list[16].main.temp_max' | awk '{print int($1+0.5)}')
+    local day3_cond=$(echo $response | jq -r '.list[16].weather[0].main')
+    
+    local day1_icon=$(get_weather_icon "$day1_cond")
+    local day2_icon=$(get_weather_icon "$day2_cond")
+    local day3_icon=$(get_weather_icon "$day3_cond")
+    
+    print_centered_row "Today" "Tomorrow" " Following"
+    print_centered_row "$day1_icon" "$day2_icon" "$day3_icon"
+    print_centered_row "${day1_temp}°F" "${day2_temp}°F" " ${day3_temp}°F"
+    print_centered_row "$day1_cond" "$day2_cond" " $day3_cond"
+}
+
+get_weather
