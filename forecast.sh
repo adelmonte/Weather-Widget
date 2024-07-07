3644d75f70a443b97f45fdfa1d085a2e0e8261bf
adelmonte <138840776+adelmonte@users.noreply.github.com>
2024-07-06 21:46:48 -0700

    Update forecast.sh

--- forecast.sh
+++ forecast.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 CITY="Spokane"
-API_KEY="433a4d1ac86863ecd165a890ece92fbc"
+API_KEY=""
 API_URL="http://api.openweathermap.org/data/2.5/forecast?q=${CITY}&cnt=24&appid=${API_KEY}&units=imperial"
 sleep 2
 
