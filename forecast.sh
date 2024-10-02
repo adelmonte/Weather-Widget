a38455ca24d885f8f175252ee4cbe75d04921f6f
adelmonte <138840776+adelmonte@users.noreply.github.com>
2024-10-01 22:47:05 -0400

    added daily highs,lows color

--- forecast.sh
+++ forecast.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
-CITY="Spokane"
-API_KEY=""
+CITY="New+York"
+API_KEY=" "
 API_URL="http://api.openweathermap.org/data/2.5/forecast?q=${CITY}&appid=${API_KEY}&units=imperial"
 sleep 2
 get_weather_icon() {
@@ -25,15 +25,32 @@ print_centered_row() {
     local col2="$2"
     local col3="$3"
     local width=16
-    printf "%s%s%s\n" "$(center_text "$col1" $width)" "$(center_text "$col2" $width)" "$(center_text "$col3" $width)"
+    echo "$(center_text "$col1" $width)$(center_text "$col2" $width)$(center_text "$col3" $width)"
+}
+print_temp_row() {
+    local high1="$1"
+    local low1="$2"
+    local high2="$3"
+    local low2="$4"
+    local high3="$5"
+    local low3="$6"
+    local width=68
+    local temp_format="<span foreground='#FF0000'>H</span>:%d° <span foreground='#0000FF'>L</span>:%d°"
+    local col1=$(printf "$temp_format" "$high1" "$low1")
+    local col2=$(printf "$temp_format" "$high2" "$low2")
+    local col3=$(printf "$temp_format" "$high3" "$low3")
+    echo "$(printf "%-${width}s" "$col1")$(center_text "$col2" $width)$(printf "%${width}s" "$col3")"
 }
 get_weather() {
     local response=$(curl -s $API_URL)
-    local day1_temp=$(echo $response | jq -r '[.list[0:8] | .[].main.temp_max] | max' | awk '{print int($1+0.5)}')
+    local day1_high=$(echo $response | jq -r '[.list[0:8] | .[].main.temp_max] | max' | awk '{print int($1+0.5)}')
+    local day1_low=$(echo $response | jq -r '[.list[0:8] | .[].main.temp_min] | min' | awk '{print int($1+0.5)}')
     local day1_cond=$(echo $response | jq -r '.list[4].weather[0].main')
-    local day2_temp=$(echo $response | jq -r '[.list[8:16] | .[].main.temp_max] | max' | awk '{print int($1+0.5)}')
+    local day2_high=$(echo $response | jq -r '[.list[8:16] | .[].main.temp_max] | max' | awk '{print int($1+0.5)}')
+    local day2_low=$(echo $response | jq -r '[.list[8:16] | .[].main.temp_min] | min' | awk '{print int($1+0.5)}')
     local day2_cond=$(echo $response | jq -r '.list[12].weather[0].main')
-    local day3_temp=$(echo $response | jq -r '[.list[16:24] | .[].main.temp_max] | max' | awk '{print int($1+0.5)}')
+    local day3_high=$(echo $response | jq -r '[.list[16:24] | .[].main.temp_max] | max' | awk '{print int($1+0.5)}')
+    local day3_low=$(echo $response | jq -r '[.list[16:24] | .[].main.temp_min] | min' | awk '{print int($1+0.5)}')
     local day3_cond=$(echo $response | jq -r '.list[20].weather[0].main')
     
     local day1_icon=$(get_weather_icon "$day1_cond")
@@ -43,9 +60,24 @@ get_weather() {
     local date2=$(date -d "1 day" +"%a %b %d")
     local date3=$(date -d "2 days" +"%a %b %d")
     
-    print_centered_row "$date1" "$date2" "$date3"
-    print_centered_row "$day1_icon" "$day2_icon" "$day3_icon"
-    print_centered_row "${day1_temp}°F" "${day2_temp}°F" "${day3_temp}°F"
-    print_centered_row "$day1_cond" "$day2_cond" "$day3_cond"
+    local row1=$(print_centered_row "$date1" "$date2" "$date3")
+    local row2=$(print_centered_row "$day1_icon" "$day2_icon" "$day3_icon")
+    local row3=$(print_temp_row "$day1_high" "$day1_low" "$day2_high" "$day2_low" "$day3_high" "$day3_low")
+    local row4=$(print_centered_row "$day1_cond" "$day2_cond" "$day3_cond")
+    
+    echo "<txt>${row1}
+${row2}
+${row3}
+${row4}</txt>"
+    echo "<tool>Weather Forecast for ${CITY}
+${date1}: ${day1_icon}
+<span foreground='#FF0000'>H</span>:${day1_high}°F <span foreground='#0000FF'>L</span>:${day1_low}°F
+Condition: ${day1_cond}
+${date2}: ${day2_icon}
+<span foreground='#FF0000'>H</span>:${day2_high}°F <span foreground='#0000FF'>L</span>:${day2_low}°F
+Condition: ${day2_cond}
+${date3}: ${day3_icon}
+<span foreground='#FF0000'>H</span>:${day3_high}°F <span foreground='#0000FF'>L</span>:${day3_low}°F
+Condition: ${day3_cond}</tool>"
 }
 get_weather
